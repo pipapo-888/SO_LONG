61e6d022e7e97237a0473d538ea7e415d8911aec
pipapo-888 <kazuya2002uk@gmail.com>
2025-10-07 23:10:31 +0900

    update: GNL submodule

--- so_long.c
+++ so_long.c
@@ -6,19 +6,29 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 17:41:39 by knomura           #+#    #+#             */
-/*   Updated: 2025/10/06 20:12:50 by knomura          ###   ########.fr       */
+/*   Updated: 2025/10/07 22:55:42 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "so_long.h"
 #include <fcntl.h>
+#include <stdio.h>
 
 
 int	main(int argc, char **argv)
 {
+	char *line;
+	
 	if (argc != 2)
 		return (0);
 	int fd;
 	fd = open(argv[1], O_RDONLY);
+
+	while (line = get_next_line(fd))
+	{
+		printf("%s", line);
+	}
+	
 
 
 	return (0);
---
+++ so_long.h
@@ -0,0 +1,8 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "GET_NEXT_LINE/get_next_line.h"
+
+char	*get_next_line(int fd);
+
+#endif
