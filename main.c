7c4222a472dbea0e8dd4d82fbe4d22cf997a97a1
pipapo-888 <kazuya2002uk@gmail.com>
2025-10-11 16:26:26 +0900

    追加

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/11 16:08:10 by knomura           #+#    #+#             */
+/*   Updated: 2025/10/11 16:08:14 by knomura          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 #include <fcntl.h>
 #include <stdio.h>
@@ -6,11 +18,23 @@
 int main (){
 	void *mlx;
 	void *win;
+	void *penguin_pic;
+	int img_width = 50;
+    int img_height = 2000;
+
 
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 800, 600, "so_long");
+	win = mlx_new_window(mlx, 800, 800, "so_long");
+
+	penguin_pic = mlx_xpm_file_to_image(mlx, "./penguin.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(mlx, win, penguin_pic, 0, 0);
 
 	mlx_loop(mlx);
 }
 
 // cc main.c minilibx-linux/libmlx.a -lXext -lX11
+
+
+// read_mapでマップ読み込み -> mapをValidate -> mlx_init -> new_windows -> 使う ->
+
+// マップ最大サイズ決まってるか
--- utilc.c
+++ utilc.c
@@ -1,26 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   utilc.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/11 16:07:46 by knomura           #+#    #+#             */
-/*   Updated: 2025/10/11 16:14:41 by knomura          ###   ########.fr       */
+/*   Created: 2025/10/11 16:07:37 by knomura           #+#    #+#             */
+/*   Updated: 2025/10/11 16:07:38 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
-
-#include <fcntl.h>
-#include <stdio.h>
-#include <string.h>
-
-# include "GET_NEXT_LINE/get_next_line.h"
-# include "minilibx-linux/mlx.h"
-# include "minilibx-linux/mlx_int.h"
-
-char	*get_next_line(int fd);
-
-#endif
