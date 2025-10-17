a9079075a909dd878cc162608c2919485ad01802
pipapo-888 <kazuya2002uk@gmail.com>
2025-10-17 17:32:39 +0900

    error

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 16:08:10 by knomura           #+#    #+#             */
-/*   Updated: 2025/10/17 16:43:11 by knomura          ###   ########.fr       */
+/*   Updated: 2025/10/17 17:30:57 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 	t_info	info;
 
 	if (argc != 2)
-		return (0);
+		put_error();
 	info.map_info = map_init(argv[1]);
 	window_render(&info);
 	(void)info;
