458587ee5e1594e1ea056e307bbb764fe04edda4
pipapo-888 <kazuya2002uk@gmail.com>
2025-10-17 17:19:57 +0900

    完成版

--- error_message.c
+++ error_message.c
@@ -6,30 +6,32 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 14:17:09 by knomura           #+#    #+#             */
-/*   Updated: 2025/10/17 16:43:41 by knomura          ###   ########.fr       */
+/*   Updated: 2025/10/17 17:18:30 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	put_error(void)
+void put_error()
 {
 	ft_putstr_fd("Error\n", 2);
 	exit(1);
 }
 
-void	put_error_free_close(int close_fd, char *line, char *message,
-		t_map *map)
+void put_error_free_close(int close_fd, char *line, char *message, t_map *map)
 {
 	int	i;
 
 	i = 0;
-	while ((size_t)i < map->height)
+	if (map->map)
 	{
-		free(map->map[i]);
-		i++;
+		while ((size_t)i < map->height && map->map[i])
+		{
+			free(map->map[i]);
+			i++;
+		}
+		free(map->map);
 	}
-	free(map->map);
 	get_next_line(-1);
 	ft_putstr_fd("Error\n", 2);
 	if (line)
